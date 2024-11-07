383ed6e2bd9b8bd7bb52333e28f6ceedaaa3dc25
Yamnyr <quentinwaroquet@gmail.com>
2024-11-07 15:24:15 +0100

    ajout d'un .env pour le port du back

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/message', (req, res) => {
     res.json({ message: 'Hello from the backend API!' });
