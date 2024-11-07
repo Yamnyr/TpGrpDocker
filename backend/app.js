1d979ad781e0a19d53a8b6de09c9e9bc4719ba4b
Yamnyr <quentinwaroquet@gmail.com>
2024-11-07 14:24:52 +0100

    config du back et de son deckerfile

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/message', (req, res) => {
+    res.json({ message: 'Hello from the backend API!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
