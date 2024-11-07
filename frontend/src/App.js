ea10777d92a7e7313b0a5f03bf2425fb449950fa
Malice776 <marinerapin@me.com>
2024-11-07 15:23:40 +0100

    FinalM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Bonjour et bienvenue sur notre projet Docker</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Yamnyr/TpGrpDocker/tree/main"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Quentin
+          - Corentin
+          - Marine
         </a>
       </header>
     </div>
